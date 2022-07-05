311ee94b95218a58c2b968c9caa33c5d74618445
Shahrear ahamed <its.shahrear@gmail.com>
2022-07-05 20:13:41 +0600

    update store value by clicking a button

--- src/Pages/Counter/Counter.js
+++ src/Pages/Counter/Counter.js
@@ -1,8 +1,10 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement } from '../../redux/state/counter';
 
 const Counter = () => {
-    const count = useSelector((state) => state.counter.value)
+    const count = useSelector((state) => state.counter.value);
+    const dispatch = useDispatch();
     return (
         <div className="rounded-3xl shadow-md w-[600px]">
             <div className="bg-slate-500 rounded-t-3xl py-3 px-1">
@@ -12,10 +14,10 @@ const Counter = () => {
             </div>
             <h2 className="text-3xl py-5 pl-5">{count}</h2>
             <div className="space-x-5 pl-5 pb-5">
-                <button className="bg-green-500 text-white px-4 py-1 rounded-md">
+                <button onClick={() => dispatch(increment())} className="bg-green-500 text-white px-4 py-1 rounded-md">
                     Increase
                 </button>
-                <button className="bg-red-500 text-white px-4 py-1 rounded-md">
+                <button onClick={() => dispatch(decrement())} className="bg-red-500 text-white px-4 py-1 rounded-md">
                     Decrease
                 </button>
             </div>
